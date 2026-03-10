2963b1285e281f7d9b21746a69d20596f77ec24b
Ikaro Oliveira <166628310+ikarozao@users.noreply.github.com>
2026-03-10 09:59:30 -0300

    teste email

--- auth.js
+++ auth.js
@@ -21,3 +21,30 @@ async function logout() {
     await supabaseClient.auth.signOut();
     window.location.href = "index.html";
 }
+
+emailjs.init("SUA_PUBLIC_KEY");
+
+const botao = document.getElementById("enviarEmail");
+
+botao.addEventListener("click", () => {
+
+    const mensagem = document.getElementById("mensagem").value;
+
+    const user = supabase.auth.getUser(); // usuário logado
+
+    const templateParams = {
+        subject: "Ajuda no Escorphy",
+        message: mensagem,
+        user_email: user.email
+    };
+
+    emailjs.send("SEU_SERVICE_ID", "SEU_TEMPLATE_ID", templateParams)
+        .then(() => {
+            alert("Email enviado com sucesso!");
+        })
+        .catch((error) => {
+            console.error(error);
+            alert("Erro ao enviar email");
+        });
+
+});
